368acd88a8d820844909b174a2e7af26831ef5f7
fuadmhnr <fuadmuhammadn@gmail.com>
2023-07-18 15:41:28 +0700

    data type primitive

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function(): void {
+  it('should must declare', function(): void {
+    const name: string = 'Fuad Muhammad Nur';
+    const balance: number = 1000;
+    const isMarried: boolean = false;
+
+    console.info(name);
+    console.info(balance);
+    console.info(isMarried);
+  });
+});
